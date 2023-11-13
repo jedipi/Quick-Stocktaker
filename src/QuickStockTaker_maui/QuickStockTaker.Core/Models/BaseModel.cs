6af31e7cb050fe702911799a8ba6984277c769e0
Kindeman <>
2023-11-13 22:36:05 +1100

    add 2 more projects

---
+++ src/QuickStockTaker_maui/QuickStockTaker.Core/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace B2CUtilities.CoreMaui.SQLiteModel
+{
+    /// <summary>
+    /// the base model for SQLite entities
+    /// </summary>
+    public class BaseModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+    }
+}
---
+++ src/QuickStockTaker_maui/QuickStockTaker.ViewModel/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace QuickStockTaker.ViewModel
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
