d811f990ecb67adc7b179ad9617c75c05477e0cc
Kindeman <jedipi@hotmail.com>
2023-11-30 14:05:35 +1100

    email settings

--- src/QuickStockTaker_maui/QuickStockTaker/Views/EmailSettingPage.xaml.cs
+++ src/QuickStockTaker_maui/QuickStockTaker/Views/EmailSettingPage.xaml.cs
@@ -9,4 +9,14 @@ public partial class EmailSettingPage : ContentPage
 		InitializeComponent();
 		BindingContext = vm;
 	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+
+    }
+
+    private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+    {
+        var a = "asdf";
+    }
 }
