8f54a5b4894bb42e99e7e37b15435885ab8352fc
Kindeman <jedipi@hotmail.com>
2024-03-23 14:43:02 +1100

    remove xamarin form project

--- src/QuickStockTaker/QuickStockTaker/Services/Interfaces/IJsonExport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuickStockTaker.Services.Interfaces
-{
-    /// <summary>
-    /// intermediate interface for exporting data in json format
-    /// </summary>
-    public interface IJsonExport:IDataExport
-    {
-    }
-}
